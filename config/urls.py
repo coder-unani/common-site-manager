ff77912c410f056ef7dc87003d9c54eac816b737
UNANI <coder.unani@gmail.com>
2024-05-13 03:26:10 +0900

    [feat] django admin 커스터마이징

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "ORBITCODE 관리자 시스템";
+admin.site.site_title = "SITE MANAGER";
+admin.site.index_title = "ORBITCODE 관리도구"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('members/', include('members.urls')),
     path('contents/', include('contents.urls')),
 ]
--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Content, ContentAttach, ContentCategory
+
 # Register your models here.
+admin.site.register(ContentAttach)
+admin.site.register(Content)
+admin.site.register(ContentCategory)
